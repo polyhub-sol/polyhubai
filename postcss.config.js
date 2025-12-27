a18d24dc186cc8e658025415e8911ac7b0a451e7
polyhub-sol <saison3335@gmail.com>
2025-12-27 14:11:22 -0500

    chore: improve configuration files with comments and formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
+/**
+ * PostCSS configuration for processing CSS with Tailwind and Autoprefixer.
+ * 
+ * Plugins:
+ * - tailwindcss: Processes Tailwind utility classes
+ * - autoprefixer: Adds vendor prefixes for browser compatibility
+ */
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
 
